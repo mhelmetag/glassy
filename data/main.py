01b8c63f768666db6f4a35057015472a0e24a078
Max Helmetag <mhelmetag@gmail.com>
2019-11-20 19:22:16 -0800

    add formatter, remove some stuff, fix formatting

--- data/main.py
+++ data/main.py
@@ -1,9 +1,9 @@
-# using region overview (like https://services.surfline.com/kbyg/regions/overview?spotId=5842041f4e65fad6a7708966)
+"Script for pulling frames from Surfline surfcams for machine learing"
+# using region overview
 # filter to spot
 # pull still rewindClip (later shift to camera stream from streamUrl as it's shorter, less space)
 # only pull stills from cams with nighttime is false && status.isDown is false
 
-import tempfile
 import requests
 import cv2
 
@@ -11,23 +11,21 @@ import cv2
 SPOT_IDS = ["5842041f4e65fad6a7708966"]
 
 for spot_id in SPOT_IDS:
-  overview_response = requests.get(f"https://services.surfline.com/kbyg/regions/overview?spotId={spot_id}")
-  json_overview_response = overview_response.json()
+    overview_response = requests.get(
+        f"https://services.surfline.com/kbyg/regions/overview?spotId={spot_id}")
+    json_overview_response = overview_response.json()
 
-  spots_json = json_overview_response["data"]["spots"]
-  spot = [s for s in spots_json if s["_id"] == spot_id][0]
+    spots_json = json_overview_response["data"]["spots"]
+    spot = [s for s in spots_json if s["_id"] == spot_id][0]
 
-  print(spot)
+    print(spot)
 
-  rewind_url = spot["cameras"][0]["rewindClip"]
-  stream_file = open("/Users/max/Downloads/test.mp4", "wb")
-  stream_response = requests.get(rewind_url)
-  stream_file.write(stream_response.content)
-  stream_file.close()
+    rewind_url = spot["cameras"][0]["rewindClip"]
+    stream_file = open("/Users/max/Downloads/test.mp4", "wb")
+    stream_response = requests.get(rewind_url)
+    stream_file.write(stream_response.content)
+    stream_file.close()
 
-  vidcap = vidcap = cv2.VideoCapture(stream_file.name)
-  success,image = vidcap.read()
-  cv2.imwrite("/Users/max/Downloads/test.jpg", image)
-
-
-  
+    vidcap = vidcap = cv2.VideoCapture(stream_file.name)
+    success, image = vidcap.read()
+    cv2.imwrite("/Users/max/Downloads/test.jpg", image)
